5d4ed3cb6d938c2d496df401240f074c1fc6cb08
Abhijna Kashyap <abhijna24x7@gmail.com>
2019-01-11 15:25:31 +0530

    Delete .driver.ko.cmd

--- 6_real_dd/.driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/trident/examples/dd/real_dd/driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/trident/examples/dd/real_dd/driver.ko /home/trident/examples/dd/real_dd/driver.o /home/trident/examples/dd/real_dd/driver.mod.o
