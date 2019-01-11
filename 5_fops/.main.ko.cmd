9fa545afd27638ecaefd6ae0c2ad4a2dd226e293
Abhijna Kashyap <abhijna24x7@gmail.com>
2019-01-11 15:24:52 +0530

    Delete .main.ko.cmd

--- 5_fops/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/trident/examples/dd/fops/main.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/trident/examples/dd/fops/main.ko /home/trident/examples/dd/fops/main.o /home/trident/examples/dd/fops/main.mod.o
